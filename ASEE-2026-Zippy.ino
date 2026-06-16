7442259550dff978e1f211f1708eeef166357874
Sofa Buchman <88793883+thesquidgrid@users.noreply.github.com>
2026-06-15 23:33:45 -0400

    working build

--- ASEE-2026-Zippy.ino
+++ ASEE-2026-Zippy.ino
@@ -1,17 +1,16 @@
+#include "src/DGMotor/DGMotor.h"
 
-#include "src/DGMotor/DGMotor.h"  
+#include "src/collisionSense/collisionSense.h"
 
-#include "src/collisionSense/collisionSense.h"  
+#include "src/drivebase/drivebase.h"
 
-#include "src/drivebase/drivebase.h"  
+#include "src/ColorSensor/ColorSensor.h"
 
-#include "src/ColorSensor/ColorSensor.h"  
+#include "src/mpu9250/mpu9250.h"
 
-#include "src/mpu9250/mpu9250.h"  
+#include "src/intake/intake.h"
 
-#include "src/intake/intake.h"  
-
-#include "src/gate/gate.h"  
+#include "src/gate/gate.h"
 
 #include "HardwareSerial.h"
 
@@ -37,7 +36,7 @@ void setup() {
 
   //---------Drive Motors---------//
   initMotors(115200);
-  
+
   //------------Intake------------//
   intake_init();
 
@@ -52,18 +51,21 @@ void loop() {
 
   int prev_baseline = 180;
   int baseline = 0;
+  int period = 2000;
 
-  for(int i = 0; i<6; i++){
+  for (int i = 0; i < 6; i++) {
     int time = millis();
-    
-    while((millis() - time) < 8400){
+
+    while ((millis() - time) < period) {
       delay(10);
-      steerForward(baseline,25.5,20);
+      steerForward(baseline, 20, 25.5, 255); //25.5 20 60
     }
-   
-    while( abs(updateYaw() - prev_baseline) > .05 ){
+
+    period = 1850;
+
+    while (abs(updateYaw() - prev_baseline) > .05) {
       Serial.println(updateYaw());
-      steerForward(prev_baseline, 25.5, 120);
+      steerForward(prev_baseline, 22.5, 100, 60);
     }
 
     prev_baseline = baseline;
@@ -74,38 +76,40 @@ void loop() {
   //back up
 
   int time = millis();
-    
-  while((millis() - time) < 1500){
+
+  while ((millis() - time) < 1500) {
     delay(10);
-    steerBackward(0,2.25,10);
+    steerBackward(0, 2.25, 10);
   }
 
   //ram into the corner 
   int angle = -90;
-  int leftSpd = 30;
-  int rightSpd = -230;
+  int leftSpd = 50;
+  int rightSpd = -255;
   rightWheels(rightSpd);
   leftWheels(leftSpd);
-  while( abs(updateYaw() - angle) > 2 ){
-      Serial.println(updateYaw());
-      
+  while (abs(updateYaw() - angle) > 2) {
+    Serial.println(updateYaw());
+
   }
 
   stopMotors();
-  while(1);
+  while (1);
 }
 
-void steerForward(int setpoint, float kp, float p_limit){
+//speed bounds = speed +/- p_limit (caps how much the speed can change)
+//speed: baseline speed the wheels will move at.
+void steerForward(int setpoint, float kp, float p_limit, int speed) {
   float p = next_control_output(setpoint, kp, p_limit);
 
-  int leftSpd = 60 - p;
-  int rightSpd = 60 + p;
+  int leftSpd = speed - p;
+  int rightSpd = speed + p;
 
   rightWheels(rightSpd);
   leftWheels(leftSpd);
 }
 
-void steerBackward(int setpoint, float kp, float p_limit){
+void steerBackward(int setpoint, float kp, float p_limit) {
   float p = next_control_output(setpoint, kp, p_limit);
 
   int leftSpd = 60 - p;
@@ -114,6 +118,3 @@ void steerBackward(int setpoint, float kp, float p_limit){
   rightWheels(-rightSpd);
   leftWheels(-leftSpd);
 }
-
-
-
