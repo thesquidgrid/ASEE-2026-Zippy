57ae66a318898353f920aa43e70ad8610b277b42
Sofa Buchman <88793883+thesquidgrid@users.noreply.github.com>
2026-06-19 14:05:22 -0400

    merged depositing with movement

--- ASEE-2026-Zippy.ino
+++ ASEE-2026-Zippy.ino
@@ -18,10 +18,9 @@
 
 #include "Adafruit_TCS34725.h"
 
-// straight forward is 25.5 kp and p_limit = 20
-
 int led = 13;
 const uint8_t MPU_ADDR = 0x68;
+
 float yawAngle = 0.0;      // degrees
 float gyroBiasRawZ = 0.0;  // raw LSB units
 unsigned long lastTime = 0;
@@ -29,6 +28,28 @@ unsigned long lastTime = 0;
 // Derivative state
 static float prevMeasurement   = 0.0f;
 static bool  hasPrevMeasurement = false;
+
+enum BinPosition {
+  AT_BLUE,
+  AT_WHITE,
+  AT_RED
+};
+
+BinPosition currentBin = AT_BLUE;
+
+// ---------------- TUNING ----------------
+
+const int BIN_MOVE_SPEED = 200; //60, 100
+const int ONE_BIN_TIME_MS = 300; //1750, 900
+
+// ---------------- DEBUG HELPERS ----------------
+
+const char* binToString(BinPosition bin) {
+  if (bin == AT_BLUE) return "BLUE";
+  if (bin == AT_WHITE) return "WHITE";
+  if (bin == AT_RED) return "RED";
+  return "UNKNOWN_BIN";
+}
 
 
 void setup() {
@@ -59,6 +80,9 @@ void setup() {
   //------------Gyro-------------//
   gyro_init();
   calibrateGyroZ();
+
+  //---------Color Sensor--------//
+  initColorSensor();
 }
 
 
@@ -107,8 +131,11 @@ void loop() {
 
   park(); //get in corner
 
+  dispenseAll(); 
+  Serial.println("dispenseAll() fin");
+  stopMotors();
+
   while(1);
-  
 }
 
 //------------- Steering Commands -----------//
@@ -250,4 +277,147 @@ void park(){
   stopMotors(); 
 }
 
+// ---------------- BIN MOVEMENT ----------------
 
+void blueToWhite() {
+  Serial.println("Movement: BLUE to WHITE");
+  driveForward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS);
+  stopMotors();
+  Serial.println("Arrived at WHITE.");
+}
+
+void blueToRed() {
+  Serial.println("Movement: BLUE to WHITE");
+  driveForward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS*2);
+  stopMotors();
+  Serial.println("Arrived at WHITE.");
+}
+
+void whiteToRed() {
+  Serial.println("Movement: WHITE to RED");
+  driveForward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS);
+  stopMotors();
+  Serial.println("Arrived at RED.");
+}
+
+void whiteToBlue() {
+  Serial.println("Movement: WHITE to BLUE");
+  driveBackward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS);
+  stopMotors();
+  Serial.println("Arrived at BLUE.");
+}
+
+void redToWhite() {
+  Serial.println("Movement: RED to WHITE");
+  driveBackward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS);
+  stopMotors();
+  Serial.println("Arrived at WHITE.");
+}
+
+void redToBlue() {
+  Serial.println("Movement: RED to WHITE");
+  driveBackward(BIN_MOVE_SPEED);
+  delay(ONE_BIN_TIME_MS*2);
+  stopMotors();
+  Serial.println("Arrived at WHITE.");
+}
+
+void moveToBin(BinPosition targetBin) {
+
+  if (currentBin == targetBin) {
+    Serial.println("Already at target bin. No movement.");
+    return;
+  }
+
+  if (currentBin == AT_BLUE && targetBin == AT_WHITE) {
+    blueToWhite();
+  }
+  else if (currentBin == AT_BLUE && targetBin == AT_RED) {
+    blueToRed();
+  }
+  else if (currentBin == AT_WHITE && targetBin == AT_BLUE) {
+    whiteToBlue();
+  }
+  else if (currentBin == AT_WHITE && targetBin == AT_RED) {
+    whiteToRed();
+  }
+  else if (currentBin == AT_RED && targetBin == AT_WHITE) {
+    redToWhite();
+  }
+  else if (currentBin == AT_RED && targetBin == AT_BLUE) {
+    redToBlue();
+  }
+
+  // software position update
+  currentBin = targetBin;
+
+  Serial.print("Updated current bin to: ");
+  Serial.println(binToString(currentBin));
+}
+
+// ---------------- COLOR -> BIN ----------------
+
+BinPosition colorToBin(BallColor color) {
+  if (color == BLUE) return AT_BLUE;
+  if (color == WHITE) return AT_WHITE;
+  if (color == RED) return AT_RED;
+
+  // if color is UNKNOWN, stay at current bin instead of moving
+  return currentBin;
+}
+
+// ---------------- DISPENSE LOGIC ----------------
+
+bool dispenseBall(int ballNumber) {
+
+  BallColor firstRead = readBallColor();
+
+  while(firstRead == UNKNOWN){
+     firstRead = readBallColor();
+     Serial.println("inloop");
+     delay(300);
+  }
+
+
+  Serial.print("Reading Ball: "); Serial.println(ballColorToString(firstRead));
+  BinPosition targetBin = colorToBin(firstRead);
+ 
+  
+  moveToBin(targetBin);
+  gateRoutine();
+  return true;
+}
+
+// ---------------- DISPENSE ALL ----------------
+
+void dispenseAll() {
+  int dispensedCount = 0;
+  int attemptCount = 0;
+  const int MAX_ATTEMPTS = 250; //25
+
+  // 12 originally, 250 is max limit of reads so robot doesnt loop foreer
+  while (dispensedCount < 12 && attemptCount < MAX_ATTEMPTS) {
+    attemptCount++;
+
+
+    bool actuallyDispensed = dispenseBall(dispensedCount + 1);
+
+    // actually increment dispense count if full sequence succeeded.
+    if (actuallyDispensed) {
+      dispensedCount++;
+    }
+    else {
+
+    }
+
+    delay(50);
+  }
+
+
+  stopMotors();
+}
